3aefb77546b88d3af3aa1989c53d51a06098c8f4
kieran <kierantmurphy@gmail.com>
2014-07-04 14:16:19 +0100

    Edited syntax.js. Added properties.js

---
+++ properties.js
@@ -0,0 +1,16 @@
+console.info("Hello from properties.js");
+
+var obj = {
+	name: "Blah"
+};
+
+console.info("hasOwnProperty: ", obj.hasOwnProperty("name"));
+console.info("propertyIsEnumerable: ", obj.propertyIsEnumerable("name"));
+console.info("Object.isExtensible: ", Object.isExtensible(obj));
+console.info("Object.isSealed: ", Object.isSealed(obj));
+
+var descriptor = Object.getOwnPropertyDescriptor(obj, "name");
+console.info("descriptor.enumerable: ", descriptor.enumerable);
+console.info("descriptor.configurable: ", descriptor.configurable);
+console.info("descriptor.writable: ", descriptor.writable);
+console.info("descriptor.value: ", descriptor.value);
--- syntax.js
+++ syntax.js
@@ -1,7 +1,13 @@
 console.info("Hello from syntax.js");
 
-var obj = {
-	name: "Blah"
-};
+var i = 0
 
-console.info("Has own property: ", obj.hasOwnProperty("name"));
+var nums = new Array(1,2,3);
+for (num in nums) {
+	console.info(nums[num]);
+}
+
+var letters = new Array('a', 'b', 'c');
+for (var i = 0; i < letters.length; i++) {
+	console.info(letters[i]);
+}
