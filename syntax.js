d8eccd6e1fa7084b28ce83b924641fede927fc2b
kieran <kierantmurphy@gmail.com>
2014-07-09 10:18:24 +0100

    Updated syntas.js

--- syntax.js
+++ syntax.js
@@ -31,6 +31,53 @@ console.info("The variable g_global=" + g_global);
 console.info("The variable g_global is undefined: " + (g_global == undefined));
 console.info("The variable g_global's type is: " + (typeof g_global));
 
+// CONDITIONALS
+// The if/if...else/if..else if...else statements same syntax as in Java
+if (true) {
+    console.info("Executing inside 'if' block...");
+}
+
+if (false) {
+    console.info("This block should NOT be executing!!");
+} else {
+    console.info("Executing inside 'else' block...");
+}
+
+if (false) {
+    console.info("This block should NOT be executing!!");
+} else if (true) {
+    console.info("Executing inside 'else if' block...");
+} else {
+    console.info("This block should NOT be executing!!");
+}
+
+// SWITCH
+// Use a switch to select one of many conditions instead of a huge if/else if block
+var number = Math.floor((Math.random() * 6) + 1); // generate a random number between 1 & 6
+switch(number) {
+    case 1:
+        console.info("Executing switch - gotta 1");
+        break;
+    case 2:
+        console.info("Executing switch - gotta 2");
+        break;
+    case 3:
+        console.info("Executing switch - gotta 3");
+        break;
+    case 4:
+        console.info("Executing switch - gotta 4");
+        break;
+    case 5:
+        console.info("Executing switch - gotta 5");
+        break;
+    case 6:
+        console.info("Executing switch - gotta 6");
+        break;
+    default:
+        console.info("Executing switch - got some other number!");
+        break;
+}
+
 // LOOPING
 var i = 0
 var nums = new Array(1,2,3);
