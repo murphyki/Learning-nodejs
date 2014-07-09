338c7bb837df04123173daa4e1e700bcf4e75745
kieran <kierantmurphy@gmail.com>
2014-07-09 11:49:10 +0100

    Updated syntax.js

--- syntax.js
+++ syntax.js
@@ -1,5 +1,11 @@
 console.info("Hello from syntax.js");
 
+// This is a comment...
+
+/*
+ * This is also a comment...
+*/
+
 // TYPES - Javascript has 5 basic/primitive types:
 //    undefined, null, boolean, number and string
 // There is also object and function
@@ -30,6 +36,99 @@ if (true) {
 console.info("The variable g_global=" + g_global);
 console.info("The variable g_global is undefined: " + (g_global == undefined));
 console.info("The variable g_global's type is: " + (typeof g_global));
+
+// ARRAYS
+var arr1 = new Array();
+arr1.push(1);
+arr1.push(2);
+arr1.push(3);
+console.info("Length of arr1=" + arr1.length);
+console.info("arr1 contents=" + arr1.toString());
+
+var arr2 = new Array([1,2,3, 4]);
+console.info("Length of arr2=" + arr2.length); // is 1...i.e. element 1 ia an array
+console.info("arr2 contents=" + arr2.toString());
+
+var arr3 = new Array(1,2,3, 4);
+console.info("Length of arr2=" + arr3.length);
+console.info("arr3 contents=" + arr3.toString());
+
+var arr4 = [1,2,3, 4, 5];
+console.info("Length of arr4=" + arr4.length);
+console.info("arr4 contents=" + arr4.toString());
+
+// FUNCTIONS
+function sayHello(msg) {
+    console.info("sayHello says: " + msg);
+}
+sayHello("hi");
+
+var myFunc1 = sayHello;
+myFunc1("This is myFunc1!!");
+
+var myFunc2 = function(arg1) {
+    console.info("This is an anonymous function");
+    // All functions have an arguments property
+    // It stores the arguments passed in in a pseudo array
+    console.info("arguments.length=" + arguments.length);
+    console.info("arguments=" + arguments.toString());
+    
+    // Functions are objects
+    // They also have properties and methods
+    // Functions have a length property
+    // Functions have methods: call, apply, bind (see examples later)
+    console.info("function.length=" + myFunc2.length);
+    
+    // The length property of a function defines the functions 'airity'
+    // i.e. how many parameters the function expects.
+    // In Javascript, a function can specify a parameter list or not
+    // and when a function is called, zero or more parameters can be passed
+    // to the function without a problem.
+    // The function length property will define how may we expect but the
+    // arguments property will store the actual parameters passed in...
+};
+myFunc2();
+myFunc2("hello");
+
+// OBJECTS
+var obj = new Object();
+obj.message = "Hello from obj";
+console.info("The variable obj.message=" + obj.message);
+
+var objLiteral = {
+    message: "Hello from objLiteral"
+};
+console.info("The variable objLiteral.message=" + objLiteral.message);
+
+// FUNCTION CONTEXT
+function contextPrinter(label) {
+    console.info(label + ":" + this.name);
+}
+
+var context1 = {
+    name: "context1"
+};
+
+var context2 = {
+    name: "context2"
+};
+
+this.name = "script";
+contextPrinter.call(this, "global-call");
+contextPrinter.call(context1, "context1-call");
+contextPrinter.call(context2, "context2-call");
+
+// apply works exactly like call except it takes an array of parameters
+// to the function
+contextPrinter.apply(this, ["global-apply"]);
+contextPrinter.apply(context1, ["context1-apply"]);
+contextPrinter.apply(context2, ["context2-apply"]);
+
+var contextPrinterForContext1 = contextPrinter.bind(context1);
+contextPrinterForContext1("context1-bind");
+
+var contextPrinterForContext2 = contextPrinter.bind(context2, "context2-bind");
+contextPrinterForContext2();
 
 // CONDITIONALS
 // The if/if...else/if..else if...else statements same syntax as in Java
@@ -79,6 +178,8 @@ switch(number) {
 }
 
 // LOOPING
+// Different types of loops:
+// for, for/in, while, do/while
 var i = 0
 var nums = new Array(1,2,3);
 for (num in nums) {
